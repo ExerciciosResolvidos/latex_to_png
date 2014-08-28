ba6142265f0a109e36d680f162e2f3144929017e
Ubuntu 14.04 LTS <lvasconcellos>
2014-08-28 16:59:20 -0700

    ADD dependency linux libs

--- latex_to_png.gemspec
+++ latex_to_png.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = LatexToPng::VERSION
   spec.authors       = ["luizamboni"]
   spec.email         = ["luizamboni2002@hotmail.com"]
-  spec.summary       = %q{TODO: Write a short summary. Required.}
-  spec.description   = %q{TODO: Write a longer description. Optional.}
+  spec.summary       = %q{Conversor de documentos Latex em imagens}
+  spec.description   = %q{Conversor de documentos Latex em imagens, via shell script}
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -17,6 +17,10 @@ Gem::Specification.new do |spec|
   spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
   spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
   spec.require_paths = ["lib"]
+
+  spec.requirements << "latex"
+  spec.requirements << "dvips"
+  spec.requirements << "imageMagik"
 
   spec.add_dependency 'rmagick'
   spec.add_development_dependency "bundler", "~> 1.6"
--- lib/latex_to_png.rb
+++ lib/latex_to_png.rb
@@ -8,25 +8,30 @@ require "latex_to_png/version"
 #sudo apt-get install imagemagick libmagickcore-dev
 
 module LatexToPng
-  ROOT_LIB = File.dirname __FILE__
-  class Convert
-  		attr_accessor :dirname , :filename, :basename, :png_file
+  raise "You need install ImageMagick dependency." if %x(hash convert 2>/dev/null || { echo >&2 "I require ImageMagick but it's not installed.  Aborting."; exit 1; }) == 1
+  raise "You need install latex dependency." if %x(hash latex 2>/dev/null || { echo >&2 "I require latex but it's not installed.  Aborting."; exit 1; }) == 1
+  raise "You need install dvips dependency." if %x(hash dvips 2>/dev/null || { echo >&2 "I require dvips but it's not installed.  Aborting."; exit 1; }) == 1
 
-  		def initialize filename
-  			@filename = filename
-  			@basename = File.basename filename.split(".")[0]
-  			@dirname =  File.dirname filename
-  		end
+    ROOT_LIB = File.dirname __FILE__
+    class Convert
+    		attr_accessor :dirname , :filename, :basename, :png_file
+
+    		def initialize filename
+    			@filename = filename
+    			@basename = File.basename filename.split(".")[0]
+    			@dirname =  File.dirname filename
+    		end
 
 
-  		def to_png
-      		@png_file =  open(%x(bash #{ROOT_LIB}/shell/convert.sh #{@filename}))
-    	end  
+    		def to_png
+        		@png_file =  open(%x(bash #{ROOT_LIB}/shell/convert.sh #{@filename}))
+      	end  
 
-    	private
-    	
-    	def move_to_dir
-    		"cd #{@dirname}"
-    	end 
-	end
+      	private
+      	
+      	def move_to_dir
+      		"cd #{@dirname}"
+      	end 
+  	end
 end
+
