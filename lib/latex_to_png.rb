271999e90fb7ca57e2eb647b370f80badc7dd126
Ubuntu 14.04 LTS <lvasconcellos>
2014-08-28 17:06:27 -0700

    ADD dependency linux libs

--- lib/latex_to_png.rb
+++ lib/latex_to_png.rb
@@ -8,9 +8,23 @@ require "latex_to_png/version"
 #sudo apt-get install imagemagick libmagickcore-dev
 
 module LatexToPng
-  raise "You need install ImageMagick dependency." if %x(hash convert 2>/dev/null || { echo >&2 "I require ImageMagick but it's not installed.  Aborting."; exit 1; }) == 1
-  raise "You need install latex dependency." if %x(hash latex 2>/dev/null || { echo >&2 "I require latex but it's not installed.  Aborting."; exit 1; }) == 1
-  raise "You need install dvips dependency." if %x(hash dvips 2>/dev/null || { echo >&2 "I require dvips but it's not installed.  Aborting."; exit 1; }) == 1
+  if %x(hash convert 2>/dev/null || { echo >&2 "I require ImageMagick but it's not installed.  Aborting."; exit 1; }) == 1
+    raise "You need install ImageMagick dependency."
+    abt = true
+  end
+  if %x(hash latex 2>/dev/null || { echo >&2 "I require latex but it's not installed.  Aborting."; exit 1; }) == 1
+    raise "You need install latex dependency."
+    abt = true
+  end
+  if %x(hash dvips 2>/dev/null || { echo >&2 "I require dvips but it's not installed.  Aborting."; exit 1; }) == 1
+    raise "You need install dvips dependency."
+    abt = true
+  end
+  # abort
+  if abt
+    abort
+  end
+
 
     ROOT_LIB = File.dirname __FILE__
     class Convert
